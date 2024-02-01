52df838d8e44ab2ad1062f50ec56871fa048c75b
furqan-abid <furqanabid10@gmail.com>
2024-02-02 01:13:57 +0500

    footer fixed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,93 +1,16 @@
 import Section from '@/src/shared/section/Section'
 import React from 'react'
 import styles from './Home.module.css'
+import { bookmarkCard, converFromPdf, convertToPDF, sliderCard } from './homeData'
 import Slider from './slider/Slider'
 
-const bookmarkCard = [
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-]
-const sliderCard = [
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-  {
-    img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-    title: "Edit PDF",
-    description: "Use the best online tool to edit PDFs in your browser",
-  },
-]
 const Home = () => {
   return (
     <div className={styles.home}>
       <Section heading={"Your Bookmarks"} cards={bookmarkCard}/>
-      <Slider heading={"Your Bookmarks"} cards={sliderCard}/>
-      <Section heading={"Your Bookmarks"} cards={bookmarkCard}/>
+      <Slider heading={"Convert from PDF"} cards={converFromPdf}/>
+      <Section heading={"Convert from PDF"} cards={converFromPdf}/>
+      <Section heading={"Convert to PDF"} cards={convertToPDF}/>
       
     </div>
   )
