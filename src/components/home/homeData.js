0e4649d4cd7df2fa39cad14ae543a9c4f389b92b
furqan-abid <furqanabid10@gmail.com>
2024-02-02 16:00:32 +0500

    header alignment

--- src/components/home/homeData.js
+++ src/components/home/homeData.js
@@ -1,176 +1,180 @@
 export const bookmarkCard = [
-    {
-      img:"https://images.hipdf.com/images2022/icons/editPDF.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/Crop-PDF.svg",
-      title: "Crop PDF",
-      description: "Crop PDF online to a selected area, adjust margin size swiftly. ",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/replace-text-pdf.svg",
-      title: "Replace Text",
-      description: " The easiest way to replace text online. ",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/Crop-PDF.svg",
-      title: "Crop PDF",
-      description: "Crop PDF online to a selected area, adjust margin size swiftly. ",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/Rotate-PDF.svg",
-      title: "Rotate PDF",
-      description: "Rotate and save your PDF pages online for free. ",
-    },
-  ]
-
-  export const sliderCard = [
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-    {
-      img:"https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Edit PDF",
-      description: "Use the best online tool to edit PDFs in your browser",
-    },
-  ]
+  {
+    img: "https://images.hipdf.com/images2022/icons/editPDF.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/Crop-PDF.svg",
+    title: "Crop PDF",
+    description:
+      "Crop PDF online to a selected area, adjust margin size swiftly. ",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/replace-text-pdf.svg",
+    title: "Replace Text",
+    description: " The easiest way to replace text online. ",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/Crop-PDF.svg",
+    title: "Crop PDF",
+    description:
+      "Crop PDF online to a selected area, adjust margin size swiftly. ",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/Rotate-PDF.svg",
+    title: "Rotate PDF",
+    description: "Rotate and save your PDF pages online for free. ",
+  },
+];
 
 
-  export const converFromPdf = [
-    {
-      img: "https://images.hipdf.com/images2022/icons/pdf-word.svg",
-      title: "PDF to Word",
-      description: "Easily convert PDF to Word document.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/PDF-PPT.svg",
-      title: "PDF to PPT",
-      description: "Convert PDF to Powerpoint online.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/pdf-excel.svg",
-      title: "PDF to Excel",
-      description: "Convert PDF to xls for free.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/PDF-JPG.svg",
-      title: "PDF to JPG",
-      description: "Convert PDF files to a set of optimized JPG,or TIFF images.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/PDF-TXT.svg",
-      title: "PDF to TXT",
-      description: "Convert your PDF to TXT, and extract text from your PDF.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/PDF-RTX.svg",
-      title: "PDF to RTF",
-      description: "Convert PDF to RTF online and free.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/PDF-Pages.svg",
-      title: "PDF to Pages",
-      description: "Convert PDF to Pages on Mac and Windows.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/pdf-html.svg",
-      title: "PDF to HTML",
-      description: "Convert your PDF documents to HTML web page.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/PDF-DXF.svg",
-      title: "PDF to DXF",
-      description: "The best online tool to convert PDF to DXF online.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/PDF-EPUB.svg",
-      title: "PDF to EPUB",
-      description: "Convert PDF file to EPUB ebook.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/OCR.svg",
-      title: "OCR",
-      description: "Convert your scanned PDFs and images into editable Word, Excel, and PPT.",
-    },
-  ];
-  
 
- export const convertToPDF = [
-    {
-      img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
-      title: "Word to PDF",
-      description: "The best Word to PDF converter online.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/PPT-PDF.svg",
-      title: "PPT to PDF",
-      description: "Convert Powerpoint to PDF online.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/Excel-PDF.svg",
-      title: "Excel to PDF",
-      description: "Easily Convert Excel spreadsheet to PDF.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/jpg-pdf.svg",
-      title: "JPG to PDF",
-      description: "Convert JPG, PNG, BMP, GIF and TIFF images to PDF.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/Image-in-TXT.svg",
-      title: "TXT to PDF",
-      description: "Convert TXT to PDF online for free.",
-    },
-    {
-      img: "https://images.hipdf.com/images2022/icons/Image-in-RTX.svg",
-      title: "RTF to PDF",
-      description: "Convert RTF file to PDF online for free.",
-    },
-  ];
-  
-  
-  
+export const sliderCard = [
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Edit PDF",
+    description: "Use the best online tool to edit PDFs in your browser",
+  },
+];
+
+
+
+export const converFromPdf = [
+  {
+    img: "https://images.hipdf.com/images2022/icons/pdf-word.svg",
+    title: "PDF to Word",
+    description: "Easily convert PDF to Word document.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/PDF-PPT.svg",
+    title: "PDF to PPT",
+    description: "Convert PDF to Powerpoint online.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/pdf-excel.svg",
+    title: "PDF to Excel",
+    description: "Convert PDF to xls for free.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/PDF-JPG.svg",
+    title: "PDF to JPG",
+    description: "Convert PDF files to a set of optimized JPG,or TIFF images.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/PDF-TXT.svg",
+    title: "PDF to TXT",
+    description: "Convert your PDF to TXT, and extract text from your PDF.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/PDF-RTX.svg",
+    title: "PDF to RTF",
+    description: "Convert PDF to RTF online and free.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/PDF-Pages.svg",
+    title: "PDF to Pages",
+    description: "Convert PDF to Pages on Mac and Windows.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/pdf-html.svg",
+    title: "PDF to HTML",
+    description: "Convert your PDF documents to HTML web page.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/PDF-DXF.svg",
+    title: "PDF to DXF",
+    description: "The best online tool to convert PDF to DXF online.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/PDF-EPUB.svg",
+    title: "PDF to EPUB",
+    description: "Convert PDF file to EPUB ebook.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/OCR.svg",
+    title: "OCR",
+    description:
+      "Convert your scanned PDFs and images into editable Word, Excel",
+  },
+];
+
+
+
+export const convertToPDF = [
+  {
+    img: "https://images.hipdf.com/images2022/icons/word-pdf.svg",
+    title: "Word to PDF",
+    description: "The best Word to PDF converter online.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/PPT-PDF.svg",
+    title: "PPT to PDF",
+    description: "Convert Powerpoint to PDF online.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/Excel-PDF.svg",
+    title: "Excel to PDF",
+    description: "Easily Convert Excel spreadsheet to PDF.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/jpg-pdf.svg",
+    title: "JPG to PDF",
+    description: "Convert JPG, PNG, BMP, GIF and TIFF images to PDF.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/Image-in-TXT.svg",
+    title: "TXT to PDF",
+    description: "Convert TXT to PDF online for free.",
+  },
+  {
+    img: "https://images.hipdf.com/images2022/icons/Image-in-RTX.svg",
+    title: "RTF to PDF",
+    description: "Convert RTF file to PDF online for free.",
+  },
+];
